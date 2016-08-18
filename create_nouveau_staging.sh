6f100c961861f53da437b9e2c197a40a2cafefac
Karol Herbst <karolherbst@gmail.com>
2016-08-19 00:52:49 +0200

    initial commit with a few patches

---
+++ create_nouveau_staging.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+NOUVEAU_GIT_URL=https://github.com/skeggsb/nouveau.git
+NOUVEAU_BASE_COMMIT=1d134769cc027a866ababdd652e126cbc5b8f7a2
+
+function abort {
+	echo "exiting"
+	git am --abort 2>/dev/null
+	git checkout master 2>/dev/null
+	git branch -D _tmp
+	git checkout ready 2>/dev/null
+}
+
+function die {
+	echo "failure occured"
+	exit
+}
+
+trap abort EXIT
+
+[ ! -d "nouveau" ] && git clone $NOUVEAU_GIT_URL nouveau
+
+function git {
+	$(which git) -C nouveau $@ >/dev/null
+}
+
+echo "updating repository"
+git fetch origin || die
+
+git checkout $NOUVEAU_BASE_COMMIT -b _tmp || die
+git branch -D ready 2>/dev/null
+
+for series in $(cat patch_list); do
+	for patch in $(find $series/ -type f | sort); do
+		echo "adding patch: $patch"
+		git am "../$patch" || die
+	done
+done
+
+git checkout -b ready
